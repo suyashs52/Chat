e912e691f00ef0508a3f5b16a841efe2cbc8efb1
suyashs52 <suyashs52@gmail.com>
2017-04-30 15:43:34 +0530

    basic chat using signalR asp.net

---
+++ SignalR2Service/SignalRChat .cs
@@ -0,0 +1,53 @@
+﻿using System.ServiceProcess;
+using Microsoft.AspNet.SignalR;
+using Microsoft.Owin.Hosting;
+using Owin;
+using Microsoft.Owin.Cors;
+using Microsoft.Owin;
+using Microsoft.AspNet.SignalR.Hubs;
+[assembly: OwinStartup(typeof(SignalR2Service.Startup))] 
+namespace SignalR2Service
+{
+    public partial class SignalRChat  : ServiceBase
+    {
+        public SignalRChat()
+        {
+            InitializeComponent();
+        }
+        
+        protected override void OnStart(string[] args)
+        {
+            
+            string url = "http://localhost:8080";
+            WebApp.Start(url); 
+        }
+        protected override void OnStop()
+        {
+           
+        } 
+        
+    }
+    class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseCors(CorsOptions.AllowAll);
+            app.MapSignalR();
+        }
+    }
+    [HubName("myHub")]
+    public class MyHub : Hub
+    {
+        public static void SendFromServer(string connectionId, string message)
+        {
+            var context = GlobalHost.ConnectionManager.GetHubContext<MyHub>();
+
+            context.Clients.Client(connectionId).serverresponse(message);
+        }
+
+        public void Send(string name, string message)
+        {
+            Clients.All.addMessage(name, message);
+        }
+    } 
+}
